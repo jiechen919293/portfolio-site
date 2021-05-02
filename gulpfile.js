4c9547941aad0633a4c72d84f5bcdab6c8e790d0
Jie Chen <chenjie919293@gmail.com>
2021-05-02 00:05:00 -0500

    change the images and make gulp work

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,34 @@
-const gulp = require('gulp');
 const { src, dest, parallel } = require('gulp');
+const concat = require('gulp-concat');
+const htmlReplace = require('gulp-html-replace');
+const uglify = require('gulp-uglify');
+const cleanCSS = require('gulp-clean-css');
 
-function html() {
-    return src('src/*.html').pipe(dest('dist'));
+function htmlTask() {
+    return src('src/*.html')
+        .pipe(
+            htmlReplace({
+                css: 'css/all-styles.css',
+            })
+        )
+        .pipe(dest('dist'))
 }
 
 function styles() {
-    return src('src/styles/*.css').pipe(dest('dist/css'));
+    return src('src/styles/*.css')
+        .pipe(concat('all-styles.css'))
+        .pipe(cleanCSS())
+        .pipe(dest('dist/css'));
 }
 
 function scripts() {
-    return src('src/scripts/*.js').pipe(dest('dist/js'));
+    return src('src/scripts/*.js')
+        .pipe(concat('bundle.js'))
+        .pipe(uglify())
+        .pipe(dest('dist/js'));
 }
 
 function images() {
     return src('src/images/*').pipe(dest('dist/images'));
 }
-exports.default = parallel(html, styles, scripts, images);
+exports.default = parallel(htmlTask, styles, scripts, images);
