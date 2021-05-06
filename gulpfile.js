8891de402345091dec55bfdf2f40785d85394c02
Jie Chen <chenjie919293@gmail.com>
2021-05-06 11:19:29 -0500

    gulp tools browersync  sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,20 @@
-const { src, dest, parallel } = require('gulp');
+const { src, dest, series, parallel, watch } = require('gulp');
+const browsersync = require('browser-sync');
+const cleanCSS = require('gulp-clean-css');
 const concat = require('gulp-concat');
 const htmlReplace = require('gulp-html-replace');
+const imagemin = require('gulp-imagemin')
+const sourcemaps = require('gulp-sourcemaps')
 const uglify = require('gulp-uglify');
-const cleanCSS = require('gulp-clean-css');
+
+function browserSync() {
+    return browsersync.init({
+        server: {
+            baseDir: './dist',
+        },
+        port: 3000,
+    })
+}
 
 function htmlTask() {
     return src('src/*.html')
@@ -14,21 +26,36 @@ function htmlTask() {
         .pipe(dest('dist'))
 }
 
-function styles() {
+function stylesTask() {
     return src('src/styles/*.css')
         .pipe(concat('all-styles.css'))
         .pipe(cleanCSS())
         .pipe(dest('dist/css'));
 }
 
-function scripts() {
+function scriptsTask() {
     return src('src/scripts/*.js')
         .pipe(concat('bundle.js'))
         .pipe(uglify())
         .pipe(dest('dist/js'));
 }
 
-function images() {
+function imagesTask() {
     return src('src/images/*').pipe(dest('dist/images'));
 }
-exports.default = parallel(htmlTask, styles, scripts, images);
+
+function watchFiles() {
+    watch('src/*.html', htmlTask);
+    watch('src/css/*.css', stylesTask);
+    watch('src/js/*.js', scriptsTask);
+    watch('src/images/*', imagesTask);
+}
+exports.html = htmlTask;
+exports.styles = scriptsTask;
+exports.scripts = scriptsTask;
+exports.images = imagesTask;
+exports.watch = parallel(watchFiles, browserSync);
+exports.dev = series(
+    parallel(htmlTask, scriptsTask, stylesTask, imagesTask); parallel(watchFiles, browserSync)
+)
+exports.default = parallel(htmlTask, scriptsTask, stylesTask, imagesTask);
